c67842a8df44c33a23a76dc48f8b0dd22325d9ef
Jefferson Oliveira <jefferoidtex7@gmail.com>
2023-02-17 02:20:54 -0300

    refactor: renomeia variavel de ambiente

--- src/config/sequelize.cli.js
+++ src/config/sequelize.cli.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
     dialect: 'mysql',
-    url: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.USER_DATABASE}`
+    url: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DATABASE}`
 }
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize"
 require('dotenv').config()
 
-const databaseUrl = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.USER_DATABASE}` || '';
+const databaseUrl = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DATABASE}` || '';
 export const sequelize = new Sequelize(databaseUrl, {
     define: {
         underscored: true
