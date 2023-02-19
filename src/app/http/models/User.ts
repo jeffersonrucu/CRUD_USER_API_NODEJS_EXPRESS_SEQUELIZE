c959ee1995185ee26e6da01fc3abac7feb74e5b8
Jefferson Oliveira <jefferoidtex7@gmail.com>
2023-02-19 18:13:16 -0300

    feat: ajusta classe User removendo o armazendo do ID

--- src/app/http/models/User.ts
+++ src/app/http/models/User.ts
@@ -1,64 +1,28 @@
 interface UserConstructor {
-    id: number;
     username: string;
     password: string;
     email: string;
     createdAt: Date;
     updatedAt: Date;
-}
-
-interface UserVerify {
-    id: boolean;
-    username: boolean;
-    password: boolean;
-    email: boolean;
-    createdAt: boolean;
-    updatedAt: boolean;
 }
 
 export class User {
-    id: number;
     username: string;
     password: string;
     email: string;
     createdAt: Date;
     updatedAt: Date;
 
+    /**
+     * Initializes all class variables
+     * 
+     * @param data
+    */
     constructor(data: UserConstructor) {
-        const field = this.validate(data)
-
-        
-
-        this.id = data.id;
-        this.username = data.username;
-        this.password = data.password;
-        this.email = data.email;
-        this.createdAt = data.createdAt;
-        this.updatedAt = data.updatedAt;
-    }
-
-    validate(data: UserConstructor): UserVerify {
-        let verify = {
-            id: false,
-            username: false,
-            password: false,
-            email: false,
-            createdAt: false,
-            updatedAt: false
-        };
-
-        if (data.id) verify.id = false
-
-        if (data.username) verify.username = false
-
-        if (data.password) verify.password = false
-
-        if (data.email) verify.email = false
-
-        if (data.createdAt) verify.createdAt = false
-
-        if (data.updatedAt) verify.updatedAt = false
-
-        return verify;
+        this.username = data?.username;
+        this.password = data?.password;
+        this.email = data?.email;
+        this.createdAt = data?.createdAt;
+        this.updatedAt = data?.updatedAt;
     }
 }
