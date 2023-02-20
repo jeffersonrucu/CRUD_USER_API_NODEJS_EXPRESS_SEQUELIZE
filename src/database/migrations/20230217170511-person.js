4dc9ea65d77ceb40e83f80f6a25771f605f55c24
Jefferson Oliveira <jefferoidtex7@gmail.com>
2023-02-20 13:38:38 -0300

    fix: corrige nome de tabela

--- src/database/migrations/20230217170511-person.js
+++ src/database/migrations/20230217170511-person.js
@@ -10,7 +10,7 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true
       },
-      fistName: {
+      firstName: {
         type: Sequelize.STRING,
         allowNull: false
       },
